bf624a7953b251d4efbff57268d12dab1c05b8e9
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-08-05 00:27:50 +0900

    Add files via upload

---
+++ WCF/1108_ChatClient/1108_ChatClient/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _1108_ChatClient.ServiceReference1;
+
+namespace _1108_ChatClient
+{
+    public partial class Form1 : Form, IChatCallback
+    {
+        public int Number
+        {
+            get { return int.Parse(textBox1.Text);  }
+            set { textBox1.Text = value.ToString(); }
+        }
+
+        private IChat chat;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //기존 Load 코드
+            InstanceContext site = new InstanceContext(this);
+            chat = new ChatClient(site);
+
+        }
+       
+        #region Form 이벤트 핸들러
+        //연결
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            Connect(Number);
+        }
+
+        //해제
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisConnect(Number);
+        }
+        //전송
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string msg = textBox2.Text;
+            SendData(Number, msg);
+            textBox2.Text = "";
+        }
+        #endregion
+
+        #region 사용자 정의 함수
+        private void Connect(int number)
+        {
+            try
+            {
+                //서버 접속
+                if (chat.Join(number) == false)
+                    MessageBox.Show("접속 실패");                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("접속 오류 :{0}", ex.Message);
+            }
+        }
+
+        private void DisConnect(int number)
+        {
+            try
+            {
+                chat.Leave(number);
+                listBox1.Items.Add("로그아웃되었습니다.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("나가기 오류 :{0}", ex.Message);
+            }
+        }
+
+        private void SendData(int number, string msg)
+        {
+            chat.Say(number, msg);
+        }
+        #endregion
+
+        #region IChatCallback 구현 상속
+        public void Receive(int idx, string message)
+        {
+            string msg = string.Format("[{0}] {1}", idx, message);
+            listBox1.Items.Add(msg);
+        }
+
+        public void UserEnter(int idx)
+        {
+            string msg = string.Format("{0}님이 로그인하셨습니다.", idx);
+            listBox1.Items.Add(msg);
+        }
+        #endregion
+    }
+}
---
+++ WCF/1108_ChatService/1108_ChatService/ChatService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1108_ChatService
+{
+    //델리게이트 선언
+    delegate void Chat(int idx, string msg, string type);
+
+    class ChatService : IChat
+    {        
+        // 개인용 델리게이트
+        private Chat MyChat;
+
+        //개인에게 보낼 정보를 담고 있는 델리게이트
+        private IChatCallback callback = null;
+
+        #region IChat 구현 상속
+        public bool Join(int idx)
+        {
+            MyChat = new Chat(UserHandler);            
+            callback = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+                       
+            BroadcastMessage(idx, "", "UserEnter");
+
+            return true;
+        }   
+
+        public void Say(int idx, string msg)
+        {
+            BroadcastMessage(idx, msg, "Receive");
+        }
+
+        public void Leave(int idx)
+        {
+            Chat d = null;
+            MyChat -= d;
+        }
+        #endregion
+
+        #region ChatService 기능 메서드
+        private void BroadcastMessage(int idx, string msg, string msgType)
+        {
+            MyChat.BeginInvoke(idx, msg, msgType, new AsyncCallback(EndAsync), null);
+        }
+
+        private void UserHandler(int idx, string msg, string msgType)
+        {
+            try
+            {
+                //클라이언트에게 보내기
+                switch (msgType)
+                {
+                    case "Receive":
+                        callback.Receive(idx, msg);
+                        break;
+                    case "UserEnter":
+                        callback.UserEnter(idx);
+                        break;
+                }
+            }
+            catch//에러가 발생했을 경우
+            {
+                Console.WriteLine("{0} 에러", idx);
+            }
+        }
+
+        private void EndAsync(IAsyncResult ar)
+        {
+            Chat d = null;
+            try
+            {
+                System.Runtime.Remoting.Messaging.AsyncResult asres =
+                    (System.Runtime.Remoting.Messaging.AsyncResult)ar;
+                d = ((Chat)asres.AsyncDelegate);
+                d.EndInvoke(ar);
+            }
+            catch
+            {
+            }
+        }
+
+        #endregion
+
+        #region IChatCallback 메서드의 기능
+        #endregion
+    }
+}
